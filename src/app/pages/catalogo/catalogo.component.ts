afc8a33b77b62c3b158b9404443ddbe8efe78693
Ramon F.A <ramon1181@gmail.com>
2018-11-14 17:04:24 -0200

    erro no FormGroup

--- src/app/pages/catalogo/catalogo.component.ts
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -25,7 +25,6 @@ export class CatalogoComponent implements OnInit {
   listagem;
   registros;
   dataSource;
-  itensCatalogo;
   catalogo: boolean = true;
 
   displayedColumns: string[] = ['status', 'codigo', 'descricao', 'acoes'];
@@ -35,75 +34,63 @@ export class CatalogoComponent implements OnInit {
     this.formulario = this.formBuilder.group({
       status: [null],
       codigo: [null],
-      descricao: [null]      
+      descricao: [null],
+      itens: this.formBuilder.group({
+        itensStatus: [null],
+        itensCodigo: [null],
+        itensDescricao: [null]
+      }),
     });
-
-    this.atualizaTabela();
     
-    this.formularioItensCatalogo = this.formBuilder.group({
-      id_catalogo: [null],
-      status: [null],
-      codigo: [null],
-      descricao: [null]      
-    });
+    this.atualizaTabela();
   
   }
 
   ngOnInit() {
   }
 
-  atualizaTabela(tabela: string = "tbCatalogos") {    
-    this.tabela = localStorage.getItem(tabela);
+  atualizaTabela() {    
+    this.tabela = localStorage.getItem("tbCatalogos");
     this.listagem = JSON.parse(this.tabela);
     
     if (this.listagem == null) {
       this.listagem = new Array();
     }
-
-    if (tabela == "tbCatalogos") {
-      this.dataSource = this.listagem;
-    } else {
-      this.itensCatalogo = this.listagem;
-    }
-
+    
+    this.dataSource = this.listagem;
   }
   
-  excluirRegistro(indiceRegistro: number, tabela: string = "tbCatalogos"){
+  excluirRegistro(indiceRegistro: number){
     if (confirm('Deseja excluir o registro?')) {
       this.listagem.splice(indiceRegistro, 1);
-      localStorage.setItem(tabela, JSON.stringify(this.listagem));
-      this.atualizaTabela(tabela);
+      localStorage.setItem("tbCatalogos", JSON.stringify(this.listagem));
+      this.atualizaTabela();
     }
   }
 
   /********** FUNÇÕES TABELA CATÁLOGO **********/
   inserirRegistro() {
-    this.atualizaTabela("tbCatalogos");
     if (this.id < 0) {
       this.listagem.push(this.formulario.value);
     } else {
       this.listagem[this.id] = this.formulario.value;
     }
     localStorage.setItem("tbCatalogos", JSON.stringify(this.listagem));
-    this.atualizaTabela("tbCatalogos");
+    this.atualizaTabela();
     this.resetarForm();
   }
   
   editarRegistro(indiceRegistro: number){
+    this.catalogo = !this.catalogo;
     let item = this.listagem[indiceRegistro];
     this.formulario.patchValue(item);
     this.id = indiceRegistro;
-    this.catalogo = !this.catalogo;
-    this.formularioItensCatalogo.patchValue({
-      id_catalogo: indiceRegistro
-    });
-    this.atualizaTabela("tbItensCatalogo");
+    this.atualizaTabela();
   }
 
   resetarForm() {
     this.id = -1;
     this.catalogo = true;
-    this.formularioItensCatalogo.reset();
     this.formulario.reset();
     this.atualizaTabela();
   }
