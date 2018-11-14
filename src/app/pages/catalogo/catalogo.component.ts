2b68d8b3ed4922716f9d54b8c70064661e435bc1
Ramon F.A <ramon1181@gmail.com>
2018-11-14 14:54:50 -0200

    Inseri itens no catálogo

--- src/app/pages/catalogo/catalogo.component.ts
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -17,12 +17,16 @@ export class CatalogoComponent implements OnInit {
 
   //INICIA O FORM
   formulario: FormGroup;
+  formularioItensCatalogo: FormGroup;
 
   id: number = -1;
+  id_item: number = -1;
   tabela;
   listagem;
   registros;
   dataSource;
+  itensCatalogo;
+  catalogo: boolean = true;
 
   displayedColumns: string[] = ['status', 'codigo', 'descricao', 'acoes'];
   
@@ -35,26 +39,45 @@ export class CatalogoComponent implements OnInit {
     });
 
     this.atualizaTabela();
+    
+    this.formularioItensCatalogo = this.formBuilder.group({
+      id_catalogo: [null],
+      status: [null],
+      codigo: [null],
+      descricao: [null]      
+    });
   
   }
 
   ngOnInit() {
   }
 
-  atualizaTabela() {    
-    this.tabela = localStorage.getItem("tbCatalogos");
+  atualizaTabela(tabela: string = "tbCatalogos") {    
+    this.tabela = localStorage.getItem(tabela);
     this.listagem = JSON.parse(this.tabela);
     
     if (this.listagem == null) {
       this.listagem = new Array();
     }
 
-    this.dataSource = this.listagem;
+    if (tabela == "tbCatalogos") {
+      this.dataSource = this.listagem;
+    } else {
+      this.itensCatalogo = this.listagem;
+    }
 
   }
+  
+  excluirRegistro(indiceRegistro: number, tabela: string = "tbCatalogos"){
+    if (confirm('Deseja excluir o registro?')) {
+      this.listagem.splice(indiceRegistro, 1);
+      localStorage.setItem(tabela, JSON.stringify(this.listagem));
+      this.atualizaTabela(tabela);
+    }
+  }
 
-  inserirRegistro() {
-    
+  /********** FUNÇÕES TABELA CATÁLOGO **********/
+  inserirRegistro() {    
     if (this.formulario.valid) {
       if (this.id < 0) {
         this.listagem.push(this.formulario.value);
@@ -71,18 +94,38 @@ export class CatalogoComponent implements OnInit {
     let item = this.listagem[indiceRegistro];
     this.formulario.patchValue(item);
     this.id = indiceRegistro;
-  }
-  
-  excluirRegistro(indiceRegistro: number){
-    if (confirm('Deseja excluir o registro?')) {
-      this.listagem.splice(indiceRegistro, 1);
-      localStorage.setItem("tbCatalogos", JSON.stringify(this.listagem));
-      this.atualizaTabela();
-    }
+    this.catalogo = !this.catalogo;
+    this.formularioItensCatalogo.patchValue({
+      id_catalogo: indiceRegistro
+    });
+    this.atualizaTabela("tbItensCatalogo");
   }
 
   resetarForm() {
     this.id = -1;
+    this.catalogo = true;
+    this.formularioItensCatalogo.reset();
     this.formulario.reset();
+    this.atualizaTabela();
   }
+  /********** FUNÇÕES TABELA CATÁLOGO **********/
+
+
+  /********** FUNÇÕES TABELA ITENS CATÁLOGO **********/
+  inserirRegistroItemCatalogo() {
+    if (this.id_item < 0) {
+      this.listagem.push(this.formularioItensCatalogo.value);
+    } else {
+      this.listagem[this.id] = this.formularioItensCatalogo.value;
+    }
+    localStorage.setItem("tbItensCatalogo", JSON.stringify(this.listagem));
+    this.atualizaTabela("tbItensCatalogo");
+    this.resetarFormItemCatalogo();
+  }
+
+  resetarFormItemCatalogo() {
+    this.catalogo = false;
+    this.formularioItensCatalogo.reset();
+  }
+  /********** FUNÇÕES TABELA ITENS CATÁLOGO **********/
 }
